8fddca13969222b10bd326ce578ca3f2f5ff4604
吕飞 <823063712@qq.com>
2020-11-15 18:38:27 +0800

    借款的每一步,逻辑

--- app/src/main/java/com/example/external/ui/activity/ReviewingActivity.java
+++ app/src/main/java/com/example/external/ui/activity/ReviewingActivity.java
@@ -1,9 +1,17 @@
 package com.example.external.ui.activity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.example.external.R;
 import com.example.external.base.BaseActivity;
 
 public class ReviewingActivity extends BaseActivity {
+    private TextView first_reviewing, step_tv, some_user, some_user_dollor;
+    private RelativeLayout first_show, second_reviewing;
+    private int next_int = 0;
 
     @Override
     protected int getLayout() {
@@ -12,12 +20,32 @@ public class ReviewingActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        initView();
+    }
+
+    private void initView() {
+        first_reviewing = findViewById(R.id.first_reviewing);
+        first_show = findViewById(R.id.first_show);
+        second_reviewing = findViewById(R.id.second_reviewing);
+        some_user = findViewById(R.id.some_user);
+        some_user_dollor = findViewById(R.id.some_user_dollor);
+        step_tv = findViewById(R.id.step_tv);
 
     }
 
     @Override
     protected void setClick() {
-
+        step_tv.setOnClickListener(v -> {
+            if (next_int == 0) {
+                first_show.setVisibility(View.GONE);
+                second_reviewing.setVisibility(View.VISIBLE);
+                first_reviewing.setText("2. Approved");
+                next_int = 1;
+            } else {
+                Intent intent = new Intent(mActivity, GetMoneyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
